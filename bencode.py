d72a99c32e4773e76a825ba818ab74624926d99c
clasch <25489225+sle7in@users.noreply.github.com>
2020-05-17 15:32:21 -0600

    Added config files for local drivers.

--- bencode.py
+++ bencode.py
@@ -4,9 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 
-service = Service('C:/Users/ben4m/Repos/proxy_jumper/chromedriver.exe')
+import config
+
+service = Service(config.chromedriver)
 service.start()
 driver = webdriver.Remote(service.service_url)
-driver.get('http://www.google.com/');
+driver.get('http://www.google.com/')
 time.sleep(5) # Let the user actually see something!
 driver.quit()
