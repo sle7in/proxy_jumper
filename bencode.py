b455f7e372e3f8a7a50a780dd7e93d1774f59aba
ben4mn <ben4mn@gmail.com>
2020-05-17 17:21:31 -0600

    fixed bencode.py to run vote

--- bencode.py
+++ bencode.py
@@ -9,6 +9,11 @@ import config
 service = Service(config.chromedriver)
 service.start()
 driver = webdriver.Remote(service.service_url)
-driver.get('http://www.google.com/')
-time.sleep(5) # Let the user actually see something!
+driver.get(config.site1)
+# elem = driver.find_element_by_class_name("polldaddy-placeholder")
+radio1 = driver.find_element_by_id("PDI_answer48849862")
+radio1.click()
+vote = driver.find_element_by_id("pd-vote-button10551560")
+vote.click()
+time.sleep(15) # quits web instance after voting
 driver.quit()
